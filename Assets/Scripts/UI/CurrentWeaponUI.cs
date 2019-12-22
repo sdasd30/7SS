ed14680a42c57edbd36470559468b9f863125b5b
sdasd30 <danielzhao2@gmail.com>
2019-12-22 18:52:17 -0500

    Added UI Elements of Weapon and HP

---
+++ Assets/Scripts/UI/CurrentWeaponUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentWeaponUI : MonoBehaviour {
+	public GameObject WeaponHandler;
+	GameObject theWeapon;
+	public Sprite weaponSprite;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		theWeapon = WeaponHandler.GetComponent<WeaponHandler> ().currWeapon;
+		weaponSprite = theWeapon.GetComponent<SpriteRenderer> ().sprite;
+		GetComponent<Image> ().sprite = weaponSprite;
+		Rect sprite = weaponSprite.rect;
+		GetComponent<RectTransform> ().localScale = new Vector3(1,sprite.height/sprite.width,0);
+	}
+}
---
+++ Assets/Scripts/UI/HPBoxes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBoxes : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject HPBox;
+    Attackable playerAtackable;
+    int maxHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAtackable = Player.GetComponent<Attackable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxHP = (int) playerAtackable.maxHP;
+        if (transform.childCount < maxHP)
+        {
+            GameObject box = Instantiate(HPBox, new Vector3(), new Quaternion());
+            box.transform.parent = this.transform;
+        }
+
+        if (transform.childCount > maxHP)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+}
