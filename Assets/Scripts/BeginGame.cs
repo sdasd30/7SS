e543a44e0af5fcfcf036578fb51f38e7b9829ea9
sdasd30 <danielzhao2@gmail.com>
2019-12-22 21:49:52 -0500

    Added an ugly title screen

---
+++ Assets/Scripts/BeginGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BeginGame : MonoBehaviour
+{
+    public void LoadTestScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    /*
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    */
+}
