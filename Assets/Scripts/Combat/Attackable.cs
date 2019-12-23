3ac38b18747a79c5d02107b475b58517809e1393
sdasd30 <danielzhao2@gmail.com>
2019-12-23 18:43:26 -0500

    Adjusted the stats of enemies, added a deathbarrier script

--- Assets/Scripts/Combat/Attackable.cs
+++ Assets/Scripts/Combat/Attackable.cs
@@ -9,8 +9,6 @@ public class Attackable : MonoBehaviour {
 	public bool allied;
 	public bool anarchy;
 	public bool alive = true;
-	public bool important;
-	public float deathbarrier = -9999;
 	public float regenRate;
 	public float regenCooldown;
 	// Use this for initialization
@@ -22,7 +20,6 @@ public class Attackable : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         checkDead();
-		FallDown ();
 		Regen ();
 	}
 
@@ -41,23 +38,11 @@ public class Attackable : MonoBehaviour {
 	public void TakeDamage(float damage){
 		hp -= damage;
 	}
+
     public void TakeKnockback(Vector2 vec)
     {
         GetComponent<PhysicsSS>().addToVelocity(vec);
     }
-	public float ReturnHP() {
-		return hp;
-	}
-	public void FallDown(){
-		if (alive && transform.position.y < deathbarrier) {
-			if (important) {
-				hp -= Mathf.RoundToInt ((hp * .25f));
-				this.transform.position = new Vector3 (0, 0, 0);
-			} else {
-				Destroy (this.gameObject);
-			}
-		}
-	}
 
 	void Regen(){
 		regenCooldown -= Time.deltaTime;
