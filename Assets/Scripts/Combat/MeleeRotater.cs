213cd401a155917d27660ba716d320efa0b5c9e3
sdasd30 <danielzhao2@gmail.com>
2019-12-28 15:36:49 -0500

    The new enemies spawn

---
+++ Assets/Scripts/Combat/MeleeRotater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeRotater : MonoBehaviour
+{
+    public bool rotated;
+    public float degreesRotation;
+    private Transform mPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPosition = GetComponent<Transform>();
+        mPosition.localRotation = Quaternion.Euler (new Vector3(0f, 0f, degreesRotation / 2f));
+    }
+
+    public void RotateMe()
+    {
+        if (rotated)
+        {
+            mPosition.localRotation = Quaternion.Euler(new Vector3(0f, 0f, degreesRotation / 2f));
+            rotated = false;
+        }
+
+        else
+        {
+            mPosition.localRotation = Quaternion.Euler(new Vector3(0f, 0f, -degreesRotation / 2f));
+            rotated = true;
+        }
+    }
+}
