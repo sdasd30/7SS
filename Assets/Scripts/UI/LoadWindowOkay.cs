0014e5f81e627d2b02d33f365f2f4178ee32d815
DrDoak <larryzhao740@yahoo.com>
2020-01-04 15:31:56 -0500

    Added Somewhat broken WebGL saving

---
+++ Assets/Scripts/UI/LoadWindowOkay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class LoadWindowOkay : MonoBehaviour
+{
+    public GameObject TextboxWindow;
+    public InputField LoadTextField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = LoadTextField.characterLimit;
+        LoadTextField.characterLimit = 32000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Confirm()
+    {
+        string data = LoadTextField.text;
+        BinaryFormatter bf = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream();
+        StreamWriter sw = new StreamWriter(ms);
+        sw.Write(data);
+        SaveObject oldSave = (SaveObject)bf.Deserialize(ms);
+        FindObjectOfType<StatTracker>().LoadFromSaveObject(oldSave);
+        Close();
+    }
+    public void Close()
+    {
+        TextboxWindow.SetActive(false);
+    }
+}
